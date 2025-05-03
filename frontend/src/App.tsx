e5d341a7aa9fa831782e5719a3897856fe8d2497
nurlybek <orozovnurlybek@gmail.com>
2025-05-03 17:44:28 +0600

    [Frontend] Импортировал контейнеры в роуты

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,18 @@ import {ToastContainer} from "react-toastify";
 import Register from "./features/users/Register.tsx";
 import Login from "./features/users/Login.tsx";
 import AppToolbar from "./UI/AppToolbar/AppToolbar.tsx";
+import FullProduct from "./features/products/FullProduct.tsx";
+import NewProduct from "./features/products/NewProduct.tsx";
+import Products from "./features/products/Products.tsx";
+import {useAppSelector} from "./app/hooks.ts";
+import {selectUser} from "./features/users/usersSlice.ts";
+import ProtectedRoute from "./UI/ProtectedRoute/ProtectedRoute.tsx";
 
 
 const App = () => {
-
+    const user = useAppSelector(selectUser);
     return (
-        <>
+         <>
             <CssBaseline />
             <ToastContainer/>
             <header>
@@ -18,8 +24,15 @@ const App = () => {
             <main>
                 <Container maxWidth="xl">
                     <Routes>
+                        <Route path="/" element={<Products/>}/>
                         <Route path="/register" element={<Register/>}/>
                         <Route path="/login" element={<Login/>}/>
+                        <Route path="/products/:id" element={<FullProduct/>}/>
+                        <Route path="/products/new" element={
+                            <ProtectedRoute isAllowed={Boolean(user)}>
+                                <NewProduct/>
+                            </ProtectedRoute>
+                        }/>
                         <Route path="*" element={<Typography variant="h4">Not found page</Typography>}/>
                     </Routes>
                 </Container>
